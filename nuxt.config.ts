9a5095cd65053e564a0c6f24882a55a1868b8e75
phmok1@naver.com <phmok1@naver.com>
2025-02-13 15:52:25 +0900

    main

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
     output: {
-      dir: 'docs' // 정적 사이트 출력 폴더를 'docs'로 변경
+      dir: 'docs', // 정적 사이트 출력 폴더를 'docs'로 변경
+      publicDir: 'docs', // public 안만들고 바로 저장
     }
   }
 })
